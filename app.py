8f32f52d61dba1ef67a67f4f047f6e479ad24d0f
SadheeraSMahanama <sadheeramahanama94@gmail.com>
2020-05-08 20:41:05 +0530

    type conversion

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
-print('Sadheera Mahanama')
-print('o----     ')
-print(' ||||')
-print('*' * 10)
-
-# variables
-price = 10
-price = 20
-rating = 4.9
-name = "Sadheera"
-name2 = 'Sadhee'
-is_published = True
-print(name2)
-print(price)
-
-patient_name = 'John Smith'
-patient_age = 20
-is_new = True
-
-# Getting Input
+# print('Sadheera Mahanama')
+# print('o----     ')
+# print(' ||||')
+# print('*' * 10)
+#
+# # variables
+# price = 10
+# price = 20
+# rating = 4.9
+# name = "Sadheera"
+# name2 = 'Sadhee'
+# is_published = True
+# print(name2)
+# print(price)
+#
+# patient_name = 'John Smith'
+# patient_age = 20
+# is_new = True
+#
+# # Getting Input
+# # name = input('What is your name? ')
+# print('Hi ' + name)
+#
+# # Exercise
 # name = input('What is your name? ')
-print('Hi ' + name)
+# fav_color = input('What is your favourite color? ')
+# print(name + ' likes ' + fav_color)
+
+# # Type Conversion
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2020 - int(birth_year)
+# print(type(age))
+# print(age)
 
 # Exercise
-name = input('What is your name? ')
-fav_color = input('What is your favourite color? ')
-print(name + ' likes ' + fav_color)
-
+weight = input('what is your weight? ')
+weight_kg = float(weight) * 0.453592
+print(weight_kg)
