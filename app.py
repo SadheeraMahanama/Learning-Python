d5bf163081570786c32b30f58b211bb48a5a1196
SadheeraSMahanama <sadheeramahanama94@gmail.com>
2020-05-08 18:07:45 +0530

    declaring variables

--- app.py
+++ app.py
@@ -2,7 +2,24 @@ print('Sadheera Mahanama')
 print('o----     ')
 print(' ||||')
 print('*' * 10)
-print("sadhee " * 3)
-print("CHanaka "*4)
-print("Let's do Python")
-print("Hello World")
+
+# variables
+price = 10
+price = 20
+rating = 4.9
+name = "Sadheera"
+name2 = 'Sadhee'
+is_published = True
+print(name2)
+print(price)
+
+age=26
+print(age)
+
+
+patient_name = 'John Smith'
+patient_age = 20
+is_new = True
+
+# Getting Input
+i
