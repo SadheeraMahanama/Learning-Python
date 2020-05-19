11ca04d9d3e91a42419b57b3aefefb84376fe601
SadheeraSMahanama <sadheeramahanama94@gmail.com>
2020-05-19 17:44:28 +0530

    Strings completed

---
+++ strings.py
@@ -0,0 +1,59 @@
+# course1 = "Python's Course for Beginners"
+# print(course1)
+#
+# course2 = 'Python for "Beginners"'
+# print(course2)
+#
+# course3 = "Python's course for 'Beginners'"
+# print(course3)
+# print(course3[0])
+#
+#
+# course4 = '''
+# Hi Sadheera,
+#
+# Here is our first email to you.
+#
+# Thank you,
+# Tools team.
+#
+# '''
+#
+# print(course4)
+#
+#
+#
+
+# course = 'Python for "Beginners"'
+# print(course[-2])  # get characters from back
+# print(course[0])
+# print(course[0:3])
+# print(course[0:])
+# print(course[1:])
+# print(course[:5])
+# # print(course[:])
+# another = course[:]
+# print(another)
+
+# Exercise
+# name = 'Sadheera'
+# print(name[1:-1])
+
+# Formatted Strings
+# first = 'Sadheera'
+# last = 'Mahanama'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}]  is a coder'
+# print(message)
+# print(msg)
+
+# String Methods
+course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course.lower())
+# print(course)
+# print(course.find('beginners'))
+# print(course.replace('D', 'b'))
+# print(course)
+print('python' in course)
